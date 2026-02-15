897ff95c64f910bd4f3353e20842976c9b617a81
Thomas Gehrmann <thgehr@gmail.com>
2026-02-15 17:15:33 +0100

    Add twob types

--- src/app/hooks/useProgram.ts
+++ src/app/hooks/useProgram.ts
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 import { Program, AnchorProvider } from '@anchor-lang/core';
 import { useConnection } from '../providers/ConnectionProvider';
 import idl from '../../idl/twob_anchor.json';
+import type { TwobAnchor } from '../../idl/twob_anchor';
 
 export function useProgram() {
   const { connection } = useConnection();
@@ -10,6 +11,6 @@ export function useProgram() {
     const provider = new AnchorProvider(connection, {} as any, {
       commitment: 'confirmed',
     });
-    return new Program(idl as any, provider);
+    return new Program<TwobAnchor>(idl as TwobAnchor, provider);
   }, [connection]);
 }
